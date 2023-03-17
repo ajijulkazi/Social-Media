b0f6d786224c57bcbb7ec0656aeb0176f31cf258
ajijul-kazi <ajijulkazi0178@gmail.com>
2023-03-18 04:42:40 +0600

    added mui, redux

--- api/server.js
+++ api/server.js
@@ -8,14 +8,22 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet);
+app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -40,6 +48,18 @@ const upload = multer({ storage });
 // ROUTES WITH FILES
 
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, createPost);
+app.get('/register', async (req, res) => {
+    const allUser = await User.find();
+    res.json(allUser);
+});
+
+
+// ROUTES
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 
 // MONGOOSE SETUP
@@ -51,5 +71,9 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true
 }).then(() => {
     app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+
+    // ADD THIS DATA ONE TIME // 
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }).catch((error) => console.log(`${error} did not connect`));
 
